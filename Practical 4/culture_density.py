a3204fa48bf775db87fe7d1b5bfe63861ee7f5e1
Teolx03 <lixian.23@intl.zju.edu.cn>
2024-05-21 00:33:39 +0800

    commit Practical 4

--- Practical 4/culture_density.py
+++ Practical 4/culture_density.py
@@ -3,7 +3,7 @@
 a = 5  # start from 5% density
 day = 0  
 print ("Day", str(day), ":",str(a)) 
-while a <=90:   # stop while the plate cell excess 90 %
+while a <=90:    # stop while the plate cell excess 90 %
     day = day+1  # add a day once the code loop (next day)
     a = a*2      # cell double every 24 hr 
     print ("Day", str(day), ":", str(a))
